4dc4b4bc1a566fa319c4faf2d2ba514c5532f176
muchlisaprian <muchlisaprian1234@gmail.com>
2024-12-06 22:44:01 +0700

    Merubah dan Menambah pertemuan ke 1 dan 5

---
+++ Prak 5/Latihan2.java
@@ -0,0 +1,41 @@
+public class Latihan2 {
+	public static void main( String []args){
+		String entry[][][] = {{{"vianslezer", "100411052", "Surabaya"},
+		{"Echa", "100411025", "Jakarta"}, {"Masayu", "100411024", "Malang"},
+		{"Revaldo", "233310004", "Bandun"}, {"Fern", "223210009", "Banjar"},
+		{"Kuin", "215610007", "Gunung Kidul"}, {"Lase", "180970003", "Riau"},
+		{"Armu", "204320005", "Kulon Progo"}}};
+		System.out.println("Nama   : "+entry[0][0][0]);
+		System.out.println("Nim    : "+entry[0][0][1]);
+		System.out.println("Alamat : "+entry[0][0][2]);
+		System.out.println("-------------------------");
+		System.out.println("Nama   : "+entry[0][1][0]);
+	    System.out.println("Nim    : "+entry[0][1][1]);
+	    System.out.println("Alamat : "+entry[0][1][2]);
+	    System.out.println("-------------------------");
+	    System.out.println("Nama   : "+entry[0][2][0]);
+	    System.out.println("Nim    : "+entry[0][2][1]);
+	    System.out.println("Alamat : "+entry[0][2][2]);
+	    System.out.println("-------------------------");
+	    System.out.println("Nama   : "+entry[0][3][0]);
+		System.out.println("Nim    : "+entry[0][3][1]);
+		System.out.println("Alamat : "+entry[0][3][2]);
+	    System.out.println("-------------------------");
+	    System.out.println("Nama   : "+entry[0][4][0]);
+		System.out.println("Nim    : "+entry[0][4][1]);
+		System.out.println("Alamat : "+entry[0][4][2]);
+	    System.out.println("-------------------------");
+	    System.out.println("Nama   : "+entry[0][5][0]);
+		System.out.println("Nim    : "+entry[0][5][1]);
+		System.out.println("Alamat : "+entry[0][5][2]);
+	    System.out.println("-------------------------");
+	    System.out.println("Nama   : "+entry[0][6][0]);
+		System.out.println("Nim    : "+entry[0][6][1]);
+		System.out.println("Alamat : "+entry[0][6][2]);
+	    System.out.println("-------------------------");
+	    System.out.println("Nama   : "+entry[0][7][0]);
+		System.out.println("Nim    : "+entry[0][7][1]);
+		System.out.println("Alamat : "+entry[0][7][2]);
+	    System.out.println("-------------------------");
+	}
+}
---
+++ Prak 5/Latihan3.java
@@ -0,0 +1,14 @@
+public class Latihan3 {
+	public static void main( String []args){
+		int entry[][][] = {{{0, 0, 0}, {0, 0, 1}, {0, 1, 0},
+		{0, 1, 1}, {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}}};
+		System.out.println("\t"+entry[0][0][0]+" | "+entry[0][0][1]+" | "+entry[0][0][2]);
+		System.out.println("\t"+entry[0][1][0]+" | "+entry[0][1][1]+" | "+entry[0][1][2]);
+	    System.out.println("\t"+entry[0][2][0]+" | "+entry[0][2][1]+" | "+entry[0][2][2]);
+	    System.out.println("\t"+entry[0][3][0]+" | "+entry[0][3][1]+" | "+entry[0][3][2]);
+	    System.out.println("\t"+entry[0][4][0]+" | "+entry[0][4][1]+" | "+entry[0][4][2]);
+	    System.out.println("\t"+entry[0][5][0]+" | "+entry[0][5][1]+" | "+entry[0][5][2]);
+	    System.out.println("\t"+entry[0][6][0]+" | "+entry[0][6][1]+" | "+entry[0][6][2]);
+	    System.out.println("\t"+entry[0][7][0]+" | "+entry[0][7][1]+" | "+entry[0][7][2]);
+	}
+}
